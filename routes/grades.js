98c84796ee2693c4a66128f2942fc6520c10fbcc
cleberfx <cleberfx@yahoo.com.br>
2020-08-06 09:29:33 +0000

    Post created

--- routes/grades.js
+++ routes/grades.js
@@ -11,13 +11,24 @@ router.get('/', async (req, res) => {
   res.send(data);
 });
 
-// router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
+  let grade = req.body;
+  let date = new Date();
+  let dateString = date.toString();
+  console.log(dateString);
+  let data = JSON.parse(await readFile('grades.json'));
+  grade = {
+    id: data.nextId++,
+    ...grade,
+    timestamp: dateString,
+  };
+  console.log(grade);
+  data.grades.push(grade);
+  console.log(grade);
+  await writeFile('grades.json', JSON.stringify(data));
 
-//   let data = JSON.parse(await readFile('grades.json'));
-  
-//   console.log(data);
-//   res.send(data);
-// });
-
+  console.log(data);
+  res.send(grade);
+});
 
 export default router;
