c86d2c273e887df728705bb09c304c0ff8d2ba13
cleberfx <cleberfx@yahoo.com.br>
2020-08-06 05:42:46 +0000

    Get created

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
-import  express from 'express';
-import gradesRouter from "./routes/grades.js"
-import { promises as fs } from "fs";
+import express from 'express';
+import gradesRouter from './routes/grades.js';
+import { promises as fs } from 'fs';
 
-const {readFile, writeFile}=fs
+const { readFile, writeFile } = fs;
 
 const app = express();
 app.use(express.json());
 
-app.use("/grades", gradesRouter );
+app.use('/grades', gradesRouter);
 
-app.listen(3000, async() => {
+app.listen(3000, async () => {
   try {
-    const initialJson={
-  nextId:1,
-  grades:[]
-
-
-}
-await readFile("grades.json")
-
-
-  
-
+    const initialJson = {
+      nextId: 1,
+      grades: [],
+    };
+    await readFile('grades.json').then(() => {
+      console.log('API Started');
+    });
   } catch (error) {
-    writeFile("grades.json",  JSON.stringify(initialJson)).then(() =>
-    {
-console.log('API Started');
-    }
-
-
-    ).catch(err =>{
-      console.log(err);
-    }
-
-    );
-    
+    await writeFile('grades.json', JSON.stringify(initialJson))
+      .then(() => {
+        console.log('API Started');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-
-
 });
-
-
--- routes/grades.js
+++ routes/grades.js
@@ -1,9 +1,23 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
+import { promises as fs } from 'fs';
 
+const { readFile, writeFile } = fs;
 
+router.get('/', async (req, res) => {
+  const data = JSON.parse(await readFile('grades.json'));
+  delete data.nextId;
+  console.log(data);
+  res.send(data);
+});
 
+// router.post('/', async (req, res) => {
 
+//   let data = JSON.parse(await readFile('grades.json'));
+  
+//   console.log(data);
+//   res.send(data);
+// });
 
 
 export default router;
